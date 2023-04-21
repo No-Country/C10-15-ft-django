dc5337a7e8ce7606d0f270630be9c820b2277180
Doliver Gonzalez <doliverpoleo15@gmail.com>
2023-04-20 20:16:18 -0500

    fixx

--- django_weather/build.sh
+++ django_weather/build.sh
@@ -4,10 +4,8 @@ set -o errexit
 
 # poetry install
 
-<<<<<<< HEAD
+
 pip install -r requirements.txt
 
-=======
->>>>>>> ed07ced204ebe1307412e89c21ddecce8df1a5fc
 python manage.py collectstatic --no-input
 python manage.py migrate
